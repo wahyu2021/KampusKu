999313b3153d3fd01a8c90f75a2fc57d8e3e9789
Wahyu2021 <wahyuwn256@gmail.com>
2025-01-06 13:36:55 +0700

    Create model database and Rename config file

---
+++ models/Database.php
@@ -0,0 +1,39 @@
+<?php
+
+class Database {
+    private $host;
+    private $dbname;
+    private $username;
+    private $password;
+    private $port;
+    private $pdo;
+
+    public function __construct(array $config) {
+        $this->host = $config['host'];
+        $this->dbname = $config['dbname'];
+        $this->username = $config['username'];
+        $this->password = $config['password'];
+        $this->port = $config['port'];
+
+        $dsn = "mysql:host={$this->host};dbname={$this->dbname};port={$this->port}";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ];
+
+        try {
+            $this->pdo = new PDO($dsn, $this->username, $this->password, $options);
+        } catch (PDOException $e) {
+            die("Koneksi gagal: " . $e->getMessage());
+        }
+    }
+
+    public function getConnection() {
+        return $this->pdo;
+    }
+
+    public function closeConnection() {
+        $this->pdo = null;
+    }
+
+}
