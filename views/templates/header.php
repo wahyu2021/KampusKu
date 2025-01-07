e87f1e5d6f428117ab694f5958b102b7d9b82517
Wahyu2021 <wahyuwn256@gmail.com>
2025-01-07 22:42:15 +0700

    fixed template

--- views/templates/header.php
+++ views/templates/header.php
@@ -7,12 +7,26 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../../assets/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <?php include_once __DIR__ . "/fonts.php" ?>
 </head>
 <body class="d-flex">
     <?php include_once __DIR__ . "/sidebar.php" ?>
 
     <div class="flex-grow-1" id="mainContent">
-        <div class="bg-second-sidebar w-full p-1">
-            <button class="btn  text-white " id="toggleSidebar">☰</button>
+        <div class="bg-second-sidebar w-full p-1 d-flex justify-content-between">
+            <button class="btn text-white fs-5" id="toggleSidebar"> <i class="bi bi-list"></i> </button>
+            <div class="d-flex gap-2">
+                <img src="../../assets/images/profile.png" alt="" width="40" height="40" class="rounded-circle">
+                <div class="dropdown">
+                    <i class="dropdown-toggle p-2 btn text-white" type="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#"> <i class="bi bi-person"></i> Profile</a></li>
+                        <li><a class="dropdown-item" href="#"> <i class="bi bi-gear"></i> Setting</a></li>
+                        <li><a class="dropdown-item" href="#"> <i class="bi bi-box-arrow-right"></i> Logout</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
+
+            
 
--- views/templates/sidebar.php
+++ views/templates/sidebar.php
@@ -1,31 +1,31 @@
 <nav id="sidebar" class="sidebar col-1 col-md-3 col-lg-2">
     <div class="d-flex flex-column p-3 h-100 bg-sidebar">
         <img src="../../assets/images/university.png" alt="" srcset="" class="img-fluid w-50 mx-auto mb-2">
-        <h5 class="text-center text-wrap text-white border-bottom pb-3">KampusKu</h5>
+        <h5 class="text-center text-wrap border-bottom pb-3" style="color: #F4FFC3;">KampusKu</h5>
         <div class="d-flex justify-content-between flex-column h-100">
-            <ul class="nav nav-pills flex-column text-center">
+            <ul class="nav nav-pills flex-column">
                 <li class="nav-item">
-                    <a href="#" class="nav-link sidebar-active text-truncate">Dashboard</a>
+                    <a href="#" class="nav-link sidebar-active text-truncate"> <i class="bi bi-building-fill"></i> Dashboard</a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link text-truncate">Dosen</a>
+                    <a href="#" class="nav-link text-truncate"> <i class="bi bi-people"></i> Dosen</a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link text-truncate">Kelas</a>
+                    <a href="#" class="nav-link text-truncate"> <i class="bi bi-book"></i> Kelas</a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link text-truncate">Jadwal Perkuliahan</a>
+                    <a href="#" class="nav-link text-truncate"> <i class="bi bi-calendar3"></i> Jadwal Perkuliahan</a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link text-truncate">Mata Kuliah</a>
+                    <a href="#" class="nav-link text-truncate"> <i class="bi bi-book"></i> Mata Kuliah</a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link text-truncate">Mahasiswa</a>
+                    <a href="#" class="nav-link text-truncate"> <i class="bi bi-people"></i> Mahasiswa</a>
                 </li>
             </ul>
             <ul class="nav nav-pills flex-column text-center">
                 <li class="nav-item">
-                    <a href="#" class="nav-link text-truncate">Logout</a>
+                    <a href="#" class="nav-link text-truncate"> <i class="bi bi-box-arrow-right"></i> Logout</a>
                 </li>
             </ul>
         </div>
